f083c6d4a7c446b80e8bf8c8d649a915714ad2c5
Yujin Yano <northwindapps@gmail.com>
2023-02-12 15:55:00 +0900

    levinsonDurbin added

--- index.js
+++ index.js
@@ -54,6 +54,53 @@ window.addEventListener('DOMContentLoaded', function() {
         return [w];
     }
 
+    function levinsonDurbin_(s,lpc_order){
+        var length_of_s=s.length;
+        var a = [];
+        var r = [];
+        var lpc = [];
+        var gamma = [];
+        var parcor = [];
+        var epsilon = [];
+        for (let index = 0; index <= lpc_order+1; index++) {
+            a.push(0);
+            r.push(0);
+            lpc.push(0);
+            gamma.push(0);
+            parcor.push(0);
+            epsilon.push(0);
+        }
+        for (let m = 1; m <= lpc_order+1; m++) {
+            for (let n = 1; index <= length_of_s-m+1; n++) {
+                r[m]=r[m]+s[n]*s[n+m-1];
+            }
+        }
+        epsilon[1]=r[1];
+        gamma[2]=-r[2]/epsilon[1];
+        lpc[1]=1;
+        lpc[2]=gamma[2];
+        epsilon[2]=epsilon[1]*(1-gamma[2]*gamma[2]);
+        for (let p = 3; p <= lpc_order+1; p++) {
+            for (let q = 1; q <= p-1; q++) {
+                a[q] = lpc[q];
+            }
+            a[p]=0;
+            var num = 0;
+            for (let q = 1; q <= p; q++) {
+                num=num+a[q]*r[p+1-q];
+            }
+            gamma[p]=-num/epsilon[p-1];
+            for (let q = 1; q <= p; q++) {
+                lpc[q]=a[q]+gamma[p]*a[p+1-q];
+            }
+            epsilon[p]=epsilon[p-1]*(1-gamma[p]*gamma[p]);
+        }
+        for (let q = 1; q <= lpc_order+1; q++) {
+            parcor[q]=-gamma[q];
+        }
+        return [lpc,parcor];
+    }
+
     function naiveRound(num, decimalPlaces = 0) {
     var p = Math.pow(10, decimalPlaces);
     return Math.round(num * p) / p;
